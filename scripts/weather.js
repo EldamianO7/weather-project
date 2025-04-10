4eefaa2d1cbb3e445c1ac0e181c2fe1e84500541
Damien <dev.eldamian0@gmail.com>
2025-04-10 13:15:31 +0200

    Added dropdown menu to select cities and added custom command (toggleCoordinatesMode). Weather and temperature now displaying.

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,6 +1,10 @@
+// Imports
+import { weatherCodes } from "../data/weatherCodes.js";
+
 // HTML elements
 const latitudeInput = document.querySelector("#latitude-input");
 const longitudeInput = document.querySelector("#longitude-input");
+const citySelector = document.querySelector("#city-dropdown");
 
 const background = document.querySelector("#weather-background");
 
@@ -9,15 +13,44 @@ const temperatureText = document.querySelector("#temperature");
 
 const icon = document.querySelector("#weather-icon");
 
+// Variables
+
 // Functions
 
 function requestApi(lat, lon) {
+  lat = lat || 0;
+  lon = lon || 0;
+
   const apiURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&forecast_days=1`;
 
-  console.log("Requesting API for weather infos.", lat, lon);
+  fetch(apiURL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => updateInterface(data))
+    .catch((error) =>
+      console.error("There was a problem with the fetch operation:", error)
+    );
 }
 
-// Events
+function updateInterface(apiData) {
+  const weatherCode = apiData.current.weather_code;
+  const temperature = apiData.current.temperature_2m;
+
+  const weather = weatherCodes.find(
+    (weatherObject) => weatherObject.code === weatherCode
+  );
+
+  console.log(weather.description, temperature);
+
+  weatherText.textContent = weather.description;
+  temperatureText.textContent = temperature;
+}
+
+// Event Functions
 
 // using this to let the time for the user to write and not spam the API when he is writing
 let timeoutId = null;
@@ -32,6 +65,21 @@ const keydownFunction = () => {
   }, 2000);
 };
 
+const citySelectorChanged = () => {
+  const [lat, lon] = citySelector.value.split(",");
+  requestApi(lat, lon);
+};
+
+// Init
+
+citySelectorChanged();
+
+// Events
+
+citySelector.onchange = (event) => {
+  citySelectorChanged();
+};
+
 latitudeInput.addEventListener("keydown", function (event) {
   keydownFunction();
 });
@@ -39,3 +87,24 @@ latitudeInput.addEventListener("keydown", function (event) {
 longitudeInput.addEventListener("keydown", function (event) {
   keydownFunction();
 });
+
+// Adding custom console commands
+
+const toggleCoordinatesMode = (bool) => {
+  if (bool) {
+    latitudeInput.classList.remove("hide");
+    longitudeInput.classList.remove("hide");
+    citySelector.classList.add("hide");
+
+    return "Coordinates mode enabled!";
+  } else {
+    latitudeInput.classList.add("hide");
+    longitudeInput.classList.add("hide");
+    citySelector.classList.remove("hide");
+
+    return "Coordinates mode disabled!";
+  }
+};
+
+// Rendre la fonction accessible globalement
+window.toggleCoordinatesMode = toggleCoordinatesMode;
